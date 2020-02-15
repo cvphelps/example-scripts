1431d6267059994a80f302685aeaf6cfbe084c9d
Carey Phelps <cvphelps@cs.stanford.edu>
2020-02-15 14:23:44 -0800

    adding a couple lines to best practices to make a nice screenshot for the docs

--- best-practices.py
+++ best-practices.py
@@ -1,10 +1,12 @@
 import wandb
 
+# At the top of your script
 config = dict (
   learning_rate = 0.01,
   momentum = 0.2,
   architecture = "CNN",
   dataset_id = "peds-0192",
+  infra = "AWS",
 )
 
 wandb.init(
@@ -13,4 +15,21 @@ wandb.init(
   tags=["baseline", "paper1"],
   config=config,
 )
+
+# Inside your training loop
+acc = 0.78
+loss = 0.21
+ped_acc = 0.62
+car_acc = 0.92
+tree_acc = 0.81
+
+wandb.log({
+  "accuracy": acc,
+  "loss": loss,
+  "class_acc": {
+    "pedestrian": ped_acc,
+    "car": car_acc,
+    "tree": tree_acc,
+  }
+})
 
